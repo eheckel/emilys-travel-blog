3388e704ec1620a7f6d06edf41b3bf7c3452352f
Emily Heckel <eheckel@gmail.com>
2019-05-28 22:30:30 +0200

    add photos

--- src/script/Data.ts
+++ src/script/Data.ts
@@ -1,18 +1,166 @@
 export default [
     {
-        "thumbSrc": "./img/big-boi-thumb.jpg",
-        "src": "./img/big-boi.jpg",
+        "thumbSrc": "./thumbs/B47C0164-1412-4379-BBD8-B238BC889FC2.JPG",
+        "src": "./img/B47C0164-1412-4379-BBD8-B238BC889FC2.JPG",
     },
     {
-        "thumbSrc": "./img/aros-thumb.jpg",
+        "thumbSrc": "./thumbs/IMG_0224.jpg",
+        "src": "./img/IMG_0224.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_0280.jpg",
+        "src": "./img/IMG_0280.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_0350.jpg",
+        "src": "./img/IMG_0350.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_0441.jpg",
+        "src": "./img/IMG_0441.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_0979.jpg",
+        "src": "./img/IMG_0979.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_1252.jpg",
+        "src": "./img/IMG_1252.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_1288.jpg",
+        "src": "./img/IMG_1288.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_1483.jpg",
+        "src": "./img/IMG_1483.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_2538.jpg",
+        "src": "./img/IMG_2538.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_3019.jpg",
+        "src": "./img/IMG_3019.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_3034.jpg",
+        "src": "./img/IMG_3034.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_3231.jpg",
+        "src": "./img/IMG_3231.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_3420.jpg",
+        "src": "./img/IMG_3420.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_3421.jpg",
+        "src": "./img/IMG_3421.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_5077.jpg",
+        "src": "./img/IMG_5077.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_5121.jpg",
+        "src": "./img/IMG_5121.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_5618.jpg",
+        "src": "./img/IMG_5618.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_5901.jpg",
+        "src": "./img/IMG_5901.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_6475.jpg",
+        "src": "./img/IMG_6475.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_7113.jpg",
+        "src": "./img/IMG_7113.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_7526.jpg",
+        "src": "./img/IMG_7526.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_8643.jpg",
+        "src": "./img/IMG_8643.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_8959.jpg",
+        "src": "./img/IMG_8959.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9467.jpg",
+        "src": "./img/IMG_9467.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9469.jpg",
+        "src": "./img/IMG_9469.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9512.jpg",
+        "src": "./img/IMG_9512.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9593.jpg",
+        "src": "./img/IMG_9593.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9594.jpg",
+        "src": "./img/IMG_9594.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9696.jpg",
+        "src": "./img/IMG_9696.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9704.jpg",
+        "src": "./img/IMG_9704.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9706.jpg",
+        "src": "./img/IMG_9706.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9775.jpg",
+        "src": "./img/IMG_9775.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9776.jpg",
+        "src": "./img/IMG_9776.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9783.jpg",
+        "src": "./img/IMG_9783.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9789.jpg",
+        "src": "./img/IMG_9789.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9825.jpg",
+        "src": "./img/IMG_9825.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/IMG_9874.jpg",
+        "src": "./img/IMG_9874.jpg",
+    },
+    {
+        "thumbSrc": "./thumbs/-8187213672245527655_IMG_6506.JPG",
+        "src": "./img/-8187213672245527655_IMG_6506.JPG",
+    },
+    {
+        "thumbSrc": "./thumbs/aros-thumb.jpg",
         "src": "./img/aros.jpg",
     },
     {
-        "thumbSrc": "./img/eagle-thumb.jpg",
+        "thumbSrc": "./thumbs/eagle-thumb.jpg",
         "src": "./img/eagle.jpg",
-    },
-    {
-        "thumbSrc": "./img/google-thumb.jpg",
-        "src": "./img/google.jpg",
     },
 ];
