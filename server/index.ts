82d265031cf3b7087fc35996bbb88e487845933e
Emily Heckel <eheckel@gmail.com>
2019-05-30 21:41:25 +0200

    rest of server side support

--- server/index.ts
+++ server/index.ts
@@ -12,8 +12,22 @@ app.engine("hbs", exphbs({
 app.use(express.static("dist/"));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { title: "Emily's Travel Blog" });
 });
+
+app.get("/about", (req, res) => {
+    res.render("about", { title: "About Me - Emily's Travel Blog" });
+});
+
+app.get("/gallery", (req, res) => {
+    res.render("gallery", { title: "Photo Gallery - Emily's Travel Blog" });
+});
+
+app.get("/todo", (req, res) => {
+    res.render("todo", { title: "Bucket List - Emily's Travel Blog" });
+});
+
+
 
 app.listen(1234, () => console.log("Listening on 1234"))
    .on("error", (e) => console.error(e));
