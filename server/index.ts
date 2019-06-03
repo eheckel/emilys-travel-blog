d7eaa44097fa086a088d62a75f8e5f01f8156fa0
Emily Heckel <eheckel@gmail.com>
2019-06-03 23:55:09 +0200

    add database

--- server/index.ts
+++ server/index.ts
@@ -1,34 +1,68 @@
-import * as express from "express";
-import * as exphbs from "express-handlebars";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import * as express from 'express';
+import * as exphbs from 'express-handlebars';
 
 
 let app = express();
-app.set("view engine", "hbs");
-app.set("views", "server/views");
-app.engine("hbs", exphbs({
-    defaultLayout: "default",
-    extname: "hbs",
+app.set('view engine', 'hbs');
+app.set('views', 'server/views');
+app.engine('hbs', exphbs({
+    defaultLayout: 'default',
+    extname: 'hbs',
 }));
-app.use(express.static("dist/"));
+app.use(express.static('dist/'));
 
-app.get("/", (req, res) => {
-    res.render("index", { title: "Emily's Travel Blog" });
+app.get('/', async (req, res) => {
+    let [rows] = await DB.query<Rows>("SELECT * FROM posts ORDER BY publishAt DESC");
+    res.render('index', {pageTitle: "Emily's Travel Blog", posts: rows});
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', { pageTitle: "About Me - Emily's Travel Blog" });
 });
 
-app.get("/about", (req, res) => {
-    res.render("about", { title: "About Me - Emily's Travel Blog" });
+app.get('/gallery', (req, res) => {
+    res.render('gallery', { pageTitle: "Photo Gallery - Emily's Travel Blog" });
 });
 
-app.get("/gallery", (req, res) => {
-    res.render("gallery", { title: "Photo Gallery - Emily's Travel Blog" });
+app.get('/todo', (req, res) => {
+    res.render('todo', { pageTitle: "Bucket List - Emily's Travel Blog" });
 });
 
-app.get("/todo", (req, res) => {
-    res.render("todo", { title: "Bucket List - Emily's Travel Blog" });
+import { DB, Rows } from './db';
+
+app.get('/todos.json', async (req, res) => {
+    let [rows] = await DB.query<Rows>("SELECT * FROM todos");
+    res.json(rows);
 });
 
-const PORT = process.env.NODE_ENV === 'production' ? 80 : 1234;
+app.get('/todos', async (req, res) => {
+    let [rows] = await DB.query<Rows>("SELECT * FROM todos");
+    res.render('todos-demo', {todos: rows});
+});
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-   .on("error", (e) => console.error(e));
+app.get('/todos/eat', async (req, res) => {
+    await DB.execute("INSERT INTO `todos` (`description`, `url`) VALUES ('Eat a hot dog', 'http://dogs4lyfe.com');", {description: "EAT!!!", url: "http://food.com"})
+    res.redirect("/todos")
+})
+
+app.get("/todos/:id", async (req, res) => {
+    let [rows] = await DB.query<Rows>('SELECT * FROM todos WHERE id=:id', {id: req.params.id});
+    res.json(rows);
+})
+
+app.get('/posts.json', async (req, res) => {
+    let [rows] = await DB.query<Rows>("SELECT * FROM posts");
+    res.json(rows);
+})
+
+export let main = async () => {
+    app.listen(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`))
+       .on('error', (e) => console.error(e));
+};
+
+main();
+
 
